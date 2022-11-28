294d98ef8dfaeeb22205fb895a6ac66000ef8ef5
Samer Mahir <samermahir@Samers-MacBook-Pro.local>
2022-11-28 10:14:45 -0800

    html and js

--- script.js
+++ script.js
@@ -1,18 +1,44 @@
+let input = document.querySelector('#input')
+
+input.addEventListener('keyup', function(event) {
+    if (event.key === 'Enter') {
+        createWeatherDisplay(event.target.value)
+    }
+})
+
 let API_KEY = 'e8713c748b04d201a89283e4f62cfbe5'
 
 function getGeoLocation(query, limit = 5) {
     return fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=${limit}&appid=${API_KEY}`)
 }
 
-function getWeather(lat, lon) {
-    return fetch(`https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}`)
+function getWeather({arguments}) {
+    return fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${arguments.lat}&lon=${arguments.lon}&units=${units}&appid=${API_KEY}`)
 }
-
-getGeoLocation('Buena Park')
-.then(response => response.json())
+function createWeatherDisplay(location){
+getGeoLocation(location)
+.then(function(response) {
+    return  response.json()
+})
 .then(data => {
-    document.body.textcontent = JSON.stringify(data, null, 2)
+    // console.log(data)
+    // let { lat, lon } = data[0]
+    getWeather({lat: data[0].lat, lon: data[0].lon})
+    .then(weatherReponse => weatherReponse.json())
+    .then(weatherData => {
+        let weatherPic = document.createElement('img')
+        weatherPic.src = `http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`
+        let currentWeatherStatement = document.createElement('p')
+        currentWeatherStatement.textContent = `${weatherData.weather[0].main}: currently ${weatherData.weather[0].description}`
+        document.body.appendChild(weatherPic)
+        document.body.appendChild(currentWeatherStatement)
+    })
+    
+    .catch(error => {
+        document.body.textcontent = error.message})
 })
 .catch(error => {
     document.body.textcontent = error.message
 })    
+
+}
